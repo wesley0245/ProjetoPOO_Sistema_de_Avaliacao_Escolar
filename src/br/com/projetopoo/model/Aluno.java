2b7d8ae1d535691656255ed31dc27346b27fd6b2
wesley <wesley.feitosa024@gmail.com>
2025-05-19 14:11:03 -0300

    Adicionar classe Aluno

--- src/br/com/projetopoo/model/Aluno.java
+++ src/br/com/projetopoo/model/Aluno.java
@@ -1,4 +1,11 @@
 package br.com.projetopoo.model;
 
 public class Aluno {
+    private String nome;
+    private String matricula;
+
+    public Aluno(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
 }
